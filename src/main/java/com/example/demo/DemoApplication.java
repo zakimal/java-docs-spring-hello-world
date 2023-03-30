026c6d853489404fdd62755aa12ecb843d1c27a6
Ozaki Yoichi (zak) <zak@inl.ics.keio.ac.jp>
2023-03-30 12:20:36 +0900

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,23 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+	@Autowired
+    private ConfigReader configReader;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@RequestMapping("/")
-	String sayHello() {
-		return "Hello World!";
-	}
+	@Override
+    public void run(String... args) {
+        System.out.println("Region = " + configReader.getRegion());
+        System.out.println("Bucket = " + configReader.getBucket());
+        System.out.println("file = " + configReader.getFile());
+    }
 }
